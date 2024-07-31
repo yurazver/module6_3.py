b564456840f91c772201fbb28a19633557e9da43
yurazver <yurazver0@gmail.com>
2024-07-31 20:43:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 class Horse:
     x_distance = 0
-    sound = 'Frrr'
+    def __init__(self, sound = 'Frrr'):
+        self.sound = sound
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
 class Eagle:
     y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self, sound = 'I train, eat, sleep, and repeat'):
+        self.sound = sound
 
     def fly(self, dy):
         self.y_distance += dy
@@ -17,7 +20,7 @@ class Pegasus(Horse, Eagle):
     def get_pos(self):
         return self.x_distance, self.y_distance
     def voice(self):
-        print(Eagle.sound)
+        print(self.sound)
 
 
 
